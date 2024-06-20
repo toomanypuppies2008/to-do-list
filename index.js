c0df30ebcba1d2dd63fac7e1f756f1f347171f33
toomanypuppies2008 <toomanypuppies@mail.ru>
2024-06-20 14:52:40 +0300

    fixed

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const taskList = document.getElementById("taskList");
 const addButton = document.getElementById("addButton");
-const taskInput = document.getElementById("taskInput"); //если эту строчку удалить с taskInput dom, то все работает также, почему?
+const taskInput = document.getElementById("taskInput");
 
 addButton.addEventListener("click", function () {
   const newLiElement = document.createElement("li");
   newLiElement.textContent = taskInput.value;
   taskList.append(newLiElement);
   taskInput.value = "";
+  newLiElement.classList.add("newLi");
 });
 
 taskList.addEventListener("click", function (event) {
-  console.log(event.target.tagName);
-  if (event.target.tagName === "LI") {
+  if (event.target.classList.contains("newLi")) {
     event.target.classList.toggle("completed");
   }
 });
